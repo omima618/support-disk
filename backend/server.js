446a7bdcabb92491d25d16484298c9a65ba371e4
Omima Khaled <85596970+omima618@users.noreply.github.com>
2022-12-03 08:23:41 +0200

    ...

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const path = require('path');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware');
@@ -12,6 +11,11 @@ const app = express();
 // CONNECT TO DATABASE
 connectDB();
 
+// START SERVER ON PORT 5000
+app.listen(PORT, () => {
+    console.log('hello from server', PORT);
+});
+
 // MIDDLE WARE
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -20,20 +24,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/tickets', require('./routes/ticketRoutes'));
 app.use('/api/tickets', require('./routes/ticketRoutes'));
-
-// SERVE FRONTEND
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../frontend/build')));
-    app.get('*', (req, res) =>
-        res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
-    );
-} else {
-    // START SERVER ON PORT 5000
-    app.listen(PORT, () => {
-        console.log('hello from server', PORT);
-    });
-}
-
 app.use(errorHandler);
 
 app.get('/', (req, res) => {
